c1e8fd5a4bbe48ca3e02d9be48877c42bccec77a
Machao <mafye@126.com>
2024-02-06 13:23:55 +0800

    add a req
    add a req

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-
+app.get('/', (req, res) => {
+    // 发送一个“Hello Express”字符串
+    res.send('Hello Express');
+});
 
 
 
